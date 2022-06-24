a582c12e9ad13600e8db25a8ae4cd34fd3528348
kelsey <enktal@gmail.com>
2022-06-23 17:10:31 -0700

    remove optimizer

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,6 @@ rustup install stable
 
 rustup target add wasm32-unknown-unknown
 
-cargo install ic-cdk-optimizer --root target
-
 echo "Deploying to the IC"
 
 dfx deploy --network=$INPUT_NETWORK $INPUT_DFX_PARAMS
